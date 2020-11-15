0fec1a4e94a5ea2356babaade554c079cc87d029
unknown <mcbrider5002@gmail.com>
2020-11-15 06:57:29 +0000

    UNTESTED - Added exact non-overlap calculation method. UNTESTED - Added some support for adding boxes one at a time and calculating non-overlap as we go. Refactored to use point class inside boxes.

--- grid.py
+++ grid.py
@@ -25,13 +25,79 @@ Find overlap of bounding boxes
 #m/z grid doesn't have to be less than half of mass tolerance
 #rt grid should be a bit less than the sampling time
 
+class Point():
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+
 class Box():
     def __init__(self, x1, x2, y1, y2):
-        self.pt1 = min(x1, x2), min(y1, y2)
-        self.pt2 = max(x1, x2), max(y1, y2)
+        self.pt1 = Point(min(x1, x2), min(y1, y2))
+        self.pt2 = Point(max(x1, x2), max(y1, y2))
         
     def __repr__(self): return "Box({} {})".format(self.pt1, self.pt2)
     def area(self): return (x2 - x1) * (y2 - y1)
+        
+class GenericBox(Box):
+    '''Makes no particular assumptions about bounding boxes.'''
+    
+    def __repr__(self): return "Generic{}".format(super().__repr__(self))
+    
+    def overlaps_with_box(self, other_box):
+        return (self.pt1.x < other_box.pt2.x and self.pt2.x > other_box.pt1.x) and (self.pt1.y < other_box.pt2.y and self.pt2.y > other_box.pt1.y)
+    
+    def contains_box(self, other_box):
+        return (
+                self.pt1.x <= other_box.pt1.x 
+                and self.pt1.y <= other_box.pt1.y 
+                and self.pt2.x >= other_box.pt2.x 
+                and self.pt2.y >= other_box.pt2.y
+               )
+               
+    def split_box(self, other_box):
+    
+        if(not self.overlaps_with_box(other_box)): return None
+    
+        left = other_box.pt1.x if other_box.pt1.x > self.pt1.x else None
+        right = other_box.pt2.x if other_box.pt2.x > self.pt2.x else None
+        low = other_box.pt1.y if other_box.pt1.y > self.pt1.y else None
+        high = other_box.pt2.y if other_box.pt2.y > self.pt2.y else None
+        
+        x1, x2, y1, y2 = self.pt1.x, self.pt2.x, self.pt1.y, self.pt2.y
+        split_boxes = []
+        if(not left is None):
+            x1 = left
+            split_boxes.append(GenericBox(self.pt1.x, x1, y1, y2))
+        if(not right is None):
+            x2 = right
+            split_boxes.append(GenericBox(x2, self.pt2.x, y1, y2))
+        if(not low is None):
+            y1 = low
+            split_boxes.append(GenericBox(x1, x2, self.pt1.y, y1))
+        if(not high is None):
+            y2 = high
+            split_boxes.append(GenericBox(x1, x2, y2, self.pt2.y))
+        if(len(split_boxes) <= 2):
+            self.pt1.x, self.pt2.x, self.pt1.y, self.pt2.y = x1, x2, y1, y2
+            return None
+        return split_boxes
+            
+class SpecialBox(Box):
+    '''Assumes all boxes have an edge at y=0 and that opposite edge is at y>0.'''
+
+    def __repr__(self): return "Special{}".format(super().__repr__(self))
+
+    def overlaps_with_box(self, other_box): 
+        return (self.pt1.x < other_box.pt2.x and self.pt2.x > other_box.pt1.x)
+
+    def contains_box(self, other_box):
+        return (
+                self.pt1.x <= other_box.pt1.x 
+                and self.pt2.x >= other_box.pt2.x 
+                and self.pt2.y >= other_box.pt2.y
+               )
+               
+    def split_box(self, other_box):
+        pass
 
 class Grid():
 
@@ -50,8 +116,8 @@ class Grid():
         self.boxes = self.init_boxes(rtboxes, mzboxes)
         
     def get_box_ranges(self, box):
-        rt_box_range = (int(box.pt1[0] / self.rt_box_size), int(box.pt2[0] / self.rt_box_size) + 1)
-        mz_box_range = (int(box.pt1[1] / self.mz_box_size), int(box.pt2[1] / self.mz_box_size) + 1)
+        rt_box_range = (int(box.pt1.x / self.rt_box_size), int(box.pt2.x / self.rt_box_size) + 1)
+        mz_box_range = (int(box.pt1.y / self.mz_box_size), int(box.pt2.y / self.mz_box_size) + 1)
         total_boxes = (rt_box_range[1] - rt_box_range[0]) * (mz_box_range[1] - mz_box_range[0])
         return rt_box_range, mz_box_range, total_boxes
         
@@ -88,8 +154,14 @@ class BoxEnv():
         self.max_mz = max_mz
         self.min_x1, self.max_x1 = min_rt, max_rt - max_xlen
         self.min_xlen, self.max_xlen, self.min_ylen, self.max_ylen = min_xlen, max_xlen, min_ylen, max_ylen
-        self.boxes_by_injection = []
+        self.injection_index = 0
+        self.boxes_by_injection = [[]]
         self.grid = None
+        
+    def set_injection_no(n): self.injection_no = n
+    def next_injection(n):
+        self.boxes_by_injection.append([])
+        self.injection_index += 1
         
     def init_grid(self, grid_class, rt_box_size, mz_box_size):
         self.grid = grid_class(self.min_rt, self.max_rt, rt_box_size, 0, self.max_mz, mz_box_size)
@@ -109,7 +181,7 @@ class BoxEnv():
         min_ylen = random.randint(100, 1000)
         max_ylen = max_mz - min_ylen
         boxenv = BoxEnv(min_rt, max_rt, max_mz, min_xlen, max_xlen, min_ylen, max_ylen)
-        for i in range(no_injections): boxenv.boxes_by_injection.append([boxenv.generate_box() for i in range(50)])
+        for i in range(no_injections): boxenv.boxes_by_injection.append([boxenv.generate_box() for j in range(50)])
         return boxenv
         
     @staticmethod
@@ -121,15 +193,21 @@ class BoxEnv():
     
     @staticmethod
     def splitting_non_overlap(box, *other_boxes):
-        '''
-        new_boxes = [box]
+        new_boxes = [box] #more efficient splitting with set?
         for b in other_boxes: #potentially filter boxes down via grid with large boxes for this loop?
-            if(overlap(box, b)): #quickly exits any box not overlapping new box
+            if(box.overlaps_with_box(b)): #quickly exits any box not overlapping new box
                 for b2 in new_boxes:
-                    #case analysis
+                    if(b.contains_box(b2)):
+                        new_boxes.remove(b2)
+                        if(not new_boxes): return 0
+                    else:
+                        split_boxes = b2.split_boxes(b)
+                        if(not split_boxes is None):
+                            new_boxes.remove(b2)
+                            new_boxes.extend(split_boxes)
         return sum(b.area() for b in new_boxes)
         
-        case analysis:
+        '''case analysis:
             - your box is contained within a previous box, in which case area is 0 (remove box from list, return if list is empty)
             - your box contains a previous box, in which case split your box into up to four new boxes, one for each edge they don't have in common (if they have all four edges in common, then box is completely overlapped)
             - neither of the previous statements are true, but boxes still overlap, then new box can contain 0, 1 or 2 of previous box's corners
@@ -138,15 +216,26 @@ class BoxEnv():
               - if 2, box overlaps part of your dimension, so you will need to split into 3
         ^could be simplified by organising by number of points
         
-        boxes could be potentially sorted by size (O(n) insert time in worst-case)
-        '''
-        pass
+        boxes could be potentially sorted by size (O(n) insert time in worst-case)'''
         
-    def box_uniqueness_by_injection(self, non_overlap):
-        return [[non_overlap(box, *self.boxes_by_injection[:i], *inj[:j]) for j, box in enumerate(inj)] for i, inj in enumerate(self.boxes_by_injection)]
+    def box_uniqueness_by_injection(self, non_overlap_f):
+        return [[non_overlap_f(box, *self.boxes_by_injection[:i], *inj[:j]) for j, box in enumerate(inj)] for i, inj in enumerate(self.boxes_by_injection)]
+        
+    class BoxScores():
+        def __init__(non_overlap):
+            self.non_overlap = non_overlap
+        
+    def add_boxes(non_overlap_f, *boxes):
+        scores = []
+        for b in boxes:
+            non_overlap = non_overlap_f(b, itertools.chain(*self.boxes_by_injection))
+            scores.append(BoxScores(non_overlap))
+            self.boxes_by_injection[self.injection_index].append(b)
+        return scores
         
 def main():
     def run_area_calcs(boxenv, rt_box_size, mz_box_size):
+        print("Run area calcs start!")
         boxenv.init_grid(DictGrid, rt_box_size, mz_box_size)
         scores_by_injection = boxenv.box_uniqueness_by_injection(boxenv.grid_non_overlap)
         print(scores_by_injection)
